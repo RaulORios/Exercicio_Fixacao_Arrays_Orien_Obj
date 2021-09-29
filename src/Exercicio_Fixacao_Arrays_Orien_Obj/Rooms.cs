2a3fbd4a173479de31e15d7b7c958dbf22b035a6
RaulORios <raul.orios89@outlook.com>
2021-09-29 13:02:55 -0300

    Arquivos adicionados

---
+++ src/Exercicio_Fixacao_Arrays_Orien_Obj/Rooms.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Exercicio_Fixacao_Arrays_Orien_Obj
+{
+    class Rooms
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        
+        public Rooms(string name, string email)
+        {
+            Name = name;
+            Email = email;            
+        }
+
+        public override string ToString()
+        {
+            return ($"{Name}; {Email}");
+        }
+
+    }
+}
